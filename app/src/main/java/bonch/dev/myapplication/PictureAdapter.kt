818bc548bbcfb5526198c0565752e28d87b795d7
Kirichenko.mv <33722704+Masya38ru@users.noreply.github.com>
2020-04-09 11:25:14 +0800

    убрал пикассо, перенес в адаптер

--- app/src/main/java/bonch/dev/myapplication/PictureAdapter.kt
+++ app/src/main/java/bonch/dev/myapplication/PictureAdapter.kt
@@ -13,8 +13,21 @@ import android.util.Log
 import android.widget.ImageView
 
 
-class PictureAdapter(var items: List<String>) :
+class PictureAdapter :
     RecyclerView.Adapter<PictureAdapter.PictureHolder>() {
+
+    private val items = listOf(
+        "https://img0.liveinternet.ru/images/attach/c/1/58/895/58895836_0_2a883_8933efd9_XL.jpg",
+        "https://cs.pikabu.ru/post_img/2013/08/08/5/1375944655_592971946.jpg",
+        "https://i.pinimg.com/736x/17/a3/a8/17a3a8381771953e5df17a6ef90cdeb3--middle-fingers-funny-pets.jpg",
+        "https://img-hw.xvideos.com/videos/profiles/profthumb/84/2d/26/brian808/profile_1_big.jpg",
+        "https://www.proza.ru/pics/2013/07/11/1100.jpg",
+        "https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkXIkzoSU89xL0yGqn8o8poaaKTM5SRkZCeTgDn6uOyic",
+        "https://www.caption.me/images/flickr/263331.jpg",
+        "https://pics.me.me/fuck-you-cat-7834808.png",
+        "https://i02.fotocdn.net/s108/2811661b2a784b71/public_pin_m/2395182812.jpg",
+        "https://i.pinimg.com/736x/2b/9a/1a/2b9a1a8becf3bfeaee73bf87ddef736a.jpg"
+    )
 
     override fun getItemId(position: Int): Long {
         return position.toLong()
